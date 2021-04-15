846bd2fd565d5cdea6e7b3e8efc9d062a00feea6
thudbuzz <graeme.harms@gmail.com>
2021-04-15 14:22:10 +0100

    grid is now interactive

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script>
+  let tableNum = 3;
 </script>
 
 <svelte:head>
@@ -6,11 +7,21 @@
 </svelte:head>
 
 <main>
+  <h1>Visualise The Times Tables</h1>
   <div class="grid">
     {#each Array(10) as _, row}
       {#each Array(10) as _, i}
-        <div class="grid-item">{i + 1 + 10 * row}</div>
+        {#if !((i + 1 + 10 * row) % tableNum)}
+          <div class="grid-item green">{i + 1 + 10 * row}</div>
+        {:else}
+          <div class="grid-item">{i + 1 + 10 * row}</div>
+        {/if}
       {/each}
+    {/each}
+  </div>
+  <div class="btn-row">
+    {#each Array(10) as _, a}
+      <button class="btn" on:click={() => (tableNum = a + 1)}>{a + 1}</button>
     {/each}
   </div>
 </main>
@@ -25,6 +36,7 @@
   .grid {
     display: grid;
     grid-template-columns: repeat(10, 1fr);
+    gap: 1px;
   }
 
   .grid-item {
@@ -34,5 +46,27 @@
     background-color: gainsboro;
     height: 35px;
     width: 35px;
+  }
+
+  .green {
+    background-color: greenyellow;
+  }
+  .btn-row {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 1px;
+    margin-top: 1em;
+  }
+
+  .btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.5rem;
+    font-size: 1rem;
+  }
+
+  h1 {
+    font-size: 1.5rem;
   }
 </style>
